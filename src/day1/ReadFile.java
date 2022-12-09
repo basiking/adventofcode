0710fd8e4d862537d6586158fa5863ad9d11f20b
basiking <33873299+basiking@users.noreply.github.com>
2022-12-09 19:17:47 +0100

    Completed part one of day one

---
+++ src/day1/ReadFile.java
@@ -0,0 +1,43 @@
+package day1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try {
+            File myObj = new File("src//day1//input.txt");
+            Scanner myReader = new Scanner(myObj);
+            ArrayList<Integer> calories = new ArrayList<>();
+            int i = 0;
+            int total = 0;
+            while (myReader.hasNextInt()) {
+                String data = myReader.nextLine();
+                if (data.equals("")) {
+                    calories.add(i);
+                    calories.set(i, total);
+                    System.out.println("Elf " + i + " has " + total + " calories.");
+                    total = 0;
+                    i++;
+                }else{
+                    int d = Integer.parseInt(data);
+                    total = total + d;
+
+                }
+
+            }
+            try {
+                int largestNumber = Collections.max(calories);
+                System.out.println("The biggest amount of calories is: " + largestNumber);
+            }catch (ClassCastException | NoSuchElementException e){
+                System.out.println("Exception caught: " + e);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/day1/day1.java
@@ -0,0 +1,8 @@
+package day1;
+public class day1 {
+
+    public static void main(String[] args) {
+        // Day 1 of advent of code
+        ReadFile.main(args);
+    }
+}
